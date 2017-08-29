8b16883f41cd99b94a78e93043f43bdce7e9726f
kathiresank806 <31157885+kathiresank806@users.noreply.github.com>
2017-08-29 16:18:16 +0530

    Create alpha.c

---
+++ alpha.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	clrscr();
+	char ch;
+	printf("Enter a character: ");
+	scanf("%c",&ch);
+	if
+	{
+		printf("%c is an alphabet",ch);
+	}
+	else
+	{
+		printf("%c is not an alphabet",ch);
+	}
+	getch();
+}
